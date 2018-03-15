2800c9d4431c36c3dd77ae343a30c34e41ee6c78
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-15 21:27:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function destructivelyRemoveFirstKitten() {
   return kittens.shift();
 }
 function appendKitten(name) {
-  var newKittens = kittens.slice(0);
+  var newKittens = [kittens.slice(0);]
   return newKittens.push(name);
 }
